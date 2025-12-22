c97ab5b4869bd0f137da1d8dace266fd43953322
jack.hewson <jack.s.hewson@gmail.com>
2025-12-22 10:28:12 +0000

    completed cpp day 1

--- 2025/1_CPP/secretEntrance.cpp
+++ 2025/1_CPP/secretEntrance.cpp
@@ -1,6 +1,49 @@
 #include <iostream>
+#include <string>
+#include <iostream>
+#include <fstream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World" << std::endl;
+    int zeroHits = 0;
+    int currentVal = 50;
+    int max = 99;
+
+    string inputText;
+
+    ifstream MyReadFile("input.txt");
+
+    while (getline(MyReadFile, inputText))
+    {
+        char direction = inputText[0];
+        int count = stoi(inputText.substr(1));
+
+        if (direction == 'R')
+        {
+            currentVal += count;
+        }
+        else
+        {
+            currentVal -= count;
+        }
+
+        // reduce the value to the range -99 to 99
+        currentVal %= 100;
+        if (currentVal < 0)
+        {
+            currentVal += 100; // if val is negative add 100 to bring it back to 0-99 range
+        }
+
+        if (currentVal == 0)
+        {
+            zeroHits++;
+        }
+
+        cout << currentVal << endl;
+    }
+
+    MyReadFile.close();
+
+    cout << zeroHits << endl;
 }
