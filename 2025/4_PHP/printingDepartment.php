12606113c70ed9df73fc814c21f5c15d66ca0de1
jack.hewson <jack.s.hewson@gmail.com>
2025-12-23 10:28:55 +0000

    working php nested forloop

--- 2025/4_PHP/printingDepartment.php
+++ 2025/4_PHP/printingDepartment.php
@@ -1,2 +1,14 @@
 <?php
-echo "Hello, World!";
+
+$test_array = array(
+    array(".", ".", "."),
+    array(".", "@", "."),
+    array(".", ".", ".")
+);
+
+foreach ($test_array as $rowIndex => $row) {
+    foreach ($row as $colIndex => $cell) {
+        echo "[$rowIndex,$colIndex] = $cell ";
+    }
+    echo PHP_EOL;
+}
