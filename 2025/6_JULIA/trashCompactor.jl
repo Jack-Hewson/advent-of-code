2f9c829a84b6825439b20a5d8289262a5d513b79
jack.hewson <jack.s.hewson@gmail.com>
2026-01-02 10:32:25 +0000

    start on day 6 julia

--- 2025/6_JULIA/trashCompactor.jl
+++ 2025/6_JULIA/trashCompactor.jl
@@ -1,1 +1,20 @@
-print("Hello World!")
+function solve(input::String)
+    lines = split(chomp(input), '\n')
+    height = length(lines)
+    width = maximum(length.(lines))
+
+    lines = [line * repeat(" ", width - length(line)) for line in lines]
+
+    total = 0
+    column = 1
+
+    while column <= width
+        println(join(column <= length(line) ? line[column] : ' ' for line in lines))
+        column += 1
+    end
+
+end
+
+input = read(joinpath(@__DIR__, "exampleInput.txt"), String)
+solve(input)
+# println(solve(input))
