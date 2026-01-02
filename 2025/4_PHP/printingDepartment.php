8a1705e64bd2fa63e6b73870db24b8a3d646f4d5
jack.hewson <jack.s.hewson@gmail.com>
2026-01-02 10:18:22 +0000

    Completion of day 5

--- 2025/4_PHP/printingDepartment.php
+++ 2025/4_PHP/printingDepartment.php
@@ -1,14 +1,38 @@
 <?php
+$lines = file('input.txt');
 
-$test_array = array(
-    array(".", ".", "."),
-    array(".", "@", "."),
-    array(".", ".", ".")
-);
+$grid = [];
 
-foreach ($test_array as $rowIndex => $row) {
-    foreach ($row as $colIndex => $cell) {
-        echo "[$rowIndex,$colIndex] = $cell ";
+$accessible = 0;
+
+foreach ($lines as $rowIndex => $line) {
+    $chars = str_split($line);
+    foreach ($chars as $charIndex => $char) {
+        if ($char == "@") {
+            $boundary_array = array(
+                $lines[$rowIndex - 1][$charIndex - 1] ?? null,
+                $lines[$rowIndex - 1][$charIndex] ?? null,
+                $lines[$rowIndex - 1][$charIndex + 1] ?? null,
+                $lines[$rowIndex][$charIndex - 1] ?? null,
+                $lines[$rowIndex][$charIndex + 1] ?? null,
+                $lines[$rowIndex + 1][$charIndex - 1] ?? null,
+                $lines[$rowIndex + 1][$charIndex] ?? null,
+                $lines[$rowIndex + 1][$charIndex + 1] ?? null,
+            );
+
+            $boundary_counter = 0;
+
+            foreach ($boundary_array as $boundary_cell) {
+                if ($boundary_cell == "@") {
+                    $boundary_counter++;
+                }
+            }
+
+            if ($boundary_counter < 4) {
+                $accessible++;
+            }
+        }
     }
-    echo PHP_EOL;
 }
+
+echo $accessible;
--- 2025/5_PYTHON/cafeteria.py
+++ 2025/5_PYTHON/cafeteria.py
@@ -1,1 +1,19 @@
-print("Hello World")
+with open("input.txt", "r") as f:
+    content = f.read().strip()
+
+part1, part2 = content.split("\n\n")
+
+freshRangeArray = part1.splitlines()
+ingredientRange = list(map(int, part2.splitlines()))
+
+freshCounter = 0
+rangeCounter = 0
+
+for ingredient in ingredientRange:
+    for freshRange in freshRangeArray:
+        minRange, maxRange = map(int, freshRange.split('-'))
+        if minRange <= ingredient <= maxRange:
+            freshCounter += 1
+            break
+        
+print("Fresh available ingredients:", freshCounter)
